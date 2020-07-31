14bd817ef3f0e8aa60d264951f16905c4c6cd25b
Jeffzieee <68960919+Jeffzieee@users.noreply.github.com>
2020-07-31 12:07:34 +0530

    Coding with Data Structures
    1. Assigning elements to lists
    2.Accessing elements from tuple
    3. Delete Ditionary elements

---
+++ Data_Structures.py
@@ -0,0 +1,17 @@
+# Assigning  List Element as value of List.
+List = ['Physics', 'chemistry', 2001, 2000]
+List.append(2054)
+print(List)
+
+#Accessing Elemennts from a Tuple
+tuple = ('Python', 'Java', 'C')
+print(tuple[1])
+
+#deleting dictionary elements
+dict = {
+    1 : 'Physics',
+    2 : 'Chemistry',
+    3 : 'Maths'
+}
+del dict[1]
+print(dict)
